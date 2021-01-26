42554cbe9fa82d75e64f5f00a71ff5cce7bfb0e1
cleandevcode <freelance1331@gmail.com>
2021-01-26 17:27:34 +0300

    update style

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import TradingViewWidget, { Themes } from "react-tradingview-widget";
 function App() {
   return (
     <div className="App">
-      <h2>BINANCE Chart</h2>
+      <h2>BINANCE: BNBUSDT Chart</h2>
       <TradingViewWidget
-        symbol="BINANCE:BNBBTC"
+        className="custom"
+        symbol="BINANCE:BNBUSDT"
         theme={Themes.DARK}
         locale="fr"
         autosize
       />
+      {/* TradingView Widget BEGIN */}
+      <div className="tradingview-widget-container">
+        <div id="tradingview_c1706"></div>
+      </div>
+      {/* TradingView Widget END */}
     </div>
   );
 }
